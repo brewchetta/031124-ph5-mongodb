5d15ec89de5be03ec914b3cb617c377e664951f3
brewchetta <chett.tiller@gmail.com>
2024-06-10 11:48:07 -0400

    some stuff woot

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-// const { searchShowById } = require('./mongo')
+const express = require('express')
+const cors = require('cors')
+
+const app = express( cors() )
+const PORT = 5555
+
+const { createMovie, showAllMovies, showMovieById } = require('./mongo')
 
 // APP ROUTES
-// goes here
+app.get('/horror-movies', async (req, res) => {
+    const movies = await showAllMovies()
+    res.json(movies)
+})
+
+
+app.get('/horror-movies/:id', async (req, res) => {
+    const id = req.params.id
+    const movie = await showMovieById(id)
+    if (movie) {
+        res.json(movie)
+    } else {
+        res.status(404)
+        res.json({'error': 'Not found'})
+    }
+})
+
+app.post('/horror-movies', express.json(), async (req, res) => {
+    await createMovie(req.body)
+    res.status(201)
+    res.send(req.body)
+})
 
 
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 const { MongoClient, ObjectId } = require('mongodb')
 const uri = process.env.MONGO_URL
 
@@ -6,23 +7,48 @@ const mongoClient = new MongoClient(uri, {
     useUnifiedTopology: true
 })
 
-const showsConnection = async () => {
+async function moviesConnection() {
     await mongoClient.connect()
-    const showsDB = mongoClient.db('showsdb')
-    const shows = showsDB.collection('shows')
-    return shows
+    const moviesDB = mongoClient.db('horrorMoviesDB')
+    const movies = moviesDB.collection('horrorMovies')
+    return movies
 }
 
 // READ ALL
 // uses shows.find({})
 
+exports.showAllMovies = async () => {
+    const moviesCursor = await moviesConnection()
+    
+    const movies = moviesCursor.find({}).toArray()
+    return movies
+}
+
 
 // READ ONE
 // uses shows.findOne({_id: ObjectId})
+exports.showMovieById = async (id) => {
+    const moviesCursor = await moviesConnection()
+
+    try {
+        const movie = await moviesCursor.findOne({ _id: new ObjectId(id) })
+        return movie
+    }
+    catch {
+        return null
+    }
+}
 
 
 // CREATE ONE
 // uses shows.insertOne(obj)
+exports.createMovie = async (movieObj) => {
+    const moviesCursor = await moviesConnection()
+
+    const newMovie = await moviesCursor.insertOne(movieObj)
+    console.log(newMovie)
+    // return newMovie
+}
 
 
 // UPDATE ONE
